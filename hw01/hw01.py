832a0d05b63b34a4e8fb715d22aafc09cb978f02
Eleanor Chi <yuechi@umich.edu>
2021-11-02 00:58:07 -0400

    Upload hw01

--- hw01/hw01.py
+++ hw01/hw01.py
@@ -10,9 +10,9 @@ def a_plus_abs_b(a, b):
     5
     """
     if b < 0:
-        f(a, b) = a-b
+        f = lambda a, b : a - b
     else:
-        f(a, b) = a+b
+        f = lambda a, b : a + b
     return f(a, b)
 
 
@@ -40,7 +40,7 @@ def two_of_three(x, y, z):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return x**2 + y**2 +z**2-max(x, y, z)**2
 
 
 def two_of_three_syntax_check():
@@ -65,12 +65,17 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    d = 1
+    for i in range(1, n - 1, 1):
+        if n % i == 0:
+            d = i
+    return d
 
 
 def limited(x, z, limit):
     """Logic that is common to invert and change."""
     if x != 0:
-        return min(z, limit)
+        return min(z / x, limit)
     else:
         return limit
 
@@ -90,7 +95,7 @@ def invert_short(x, limit):
     >>> invert_short(x, 100)  # No error, even though 1/x divides by 0!
     100
     """
-    return limited(x, 1 / x, limit)
+    return limited(x, 1, limit)
 
 
 def change_short(x, y, limit):
@@ -108,7 +113,7 @@ def change_short(x, y, limit):
     >>> change_short(x, y, 100)  # No error, even though abs(y - x) / x divides by 0!
     100
     """
-    return limited(x, abs(y - x) / x, limit)
+    return limited(x, abs(y - x), limit)
 
 
 def invert_and_change_syntax_check():
@@ -140,10 +145,22 @@ def hailstone(n):
     7
     """
     "*** YOUR CODE HERE ***"
+    i = 1
+    print(n)
 
+    while n != 1:
+        if n % 2 == 0:
+            n = int(n / 2)
+            print(n)
+            i = i + 1
+        else:
+            n = int(n * 3 + 1)
+            print(n)
+            i = i + 1
+    return i
 
 "*** YOUR CODE HERE ***"
-quine = ''
+quine = print("QUINE!")
 
 
 def quine_test():
