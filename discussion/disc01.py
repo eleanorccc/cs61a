ab4ef5a8d0f5b75a2270ad7069c2a898f539f9e2
Eleanor Chi <yuechi@umich.edu>
2021-11-16 07:19:44 -0500

    Update hw02

---
+++ discussion/disc01.py
@@ -0,0 +1,111 @@
+# Q1
+def wears_jacket_with_if(temp, raining):
+    """
+    >>> wears_jacket_with_if(90, False)
+    False
+    >>> wears_jacket_with_if(40, False)
+    True
+    >>> wears_jacket_with_if(100, True)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    return temp < 60 | raining
+
+# Q3
+def if_function(condition, true_result, false_result):
+    """Return true_result if condition is a true value, and
+    false_result otherwise.
+
+    >>> if_function(True, 2, 3)
+    2
+    >>> if_function(False, 2, 3)
+    3
+    >>> if_function(3==2, 'equal', 'not equal')
+    'not equal'
+    >>> if_function(3>2, 'bigger', 'smaller')
+    'bigger'
+    """
+    if condition:
+        return true_result
+    else:
+        return false_result
+
+def with_if_statement():
+    """
+    >>> result = with_if_statement()
+    61A
+    >>> print(result)
+    None
+    """
+    if cond():
+        return true_func()
+    else:
+        return false_func()
+
+def with_if_function():
+    """
+    >>> result = with_if_function()
+    Welcome to
+    61A
+    >>> print(result)
+    None
+    """
+    return if_function(cond(), true_func(), false_func())
+
+def cond():
+    return False
+
+def true_func():
+    print("Welcome to")
+
+def false_func():
+    print("61A")
+
+# Q5
+def is_prime(n):
+    """
+    >>> is_prime(10)
+    False
+    >>> is_prime(7)
+    True
+    """
+    t = False
+    for i in range(2, n, 1):
+        if n % i == 0:
+            t = True
+    return t
+
+# Q6
+def fizzbuzz(n):
+    """
+    >>> result = fizzbuzz(16)
+    1
+    2
+    fizz
+    4
+    buzz
+    fizz
+    7
+    8
+    fizz
+    buzz
+    11
+    fizz
+    13
+    14
+    fizzbuzz
+    16
+    >>> result == None
+    True
+    """
+    i = 1
+    while i <= n:
+        if i % 3 == 0 and i % 5 == 0:
+            print('fizzbuzz')
+        elif i % 3 == 0:
+            print('fizz')
+        elif i % 5 == 0:
+            print('buzz')
+        else:
+            print(i)
+        i += 1
